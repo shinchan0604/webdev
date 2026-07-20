be05e6f7cc5fa196b17da222b2ad0b5622f17b93
shinchan0604 <190394302+shinchan0604@users.noreply.github.com>
2026-07-20 17:07:53 +0000

    read endpoints with 404

--- todo/app.js
+++ todo/app.js
@@ -1,5 +1,24 @@
 import express from 'express';
 
+const tasks = [
+  {
+    id: 1,
+    title: "Learn Express",
+    done: false
+  },
+  {
+    id: 2,
+    title: "Build Task API",
+    done: true
+  },
+  {
+    id: 3,
+    title: "Test with curl",
+    done: false
+  }
+];
+
+
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
@@ -8,6 +27,22 @@ app.get('/', (req, res) => {
 
 app.get('/health', (req, res) => {
     res.json({ "status": "ok" });
+})
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const task = tasks.find(task => task.id === id);
+    if(!task){
+        return res.status(404).json({
+            error:`task ${id} not found`
+            
+        });
+    }
+    res.json(task);
 })
 
 app.listen(port, () => {
