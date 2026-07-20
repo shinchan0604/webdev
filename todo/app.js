b7283ef241b2e56a325280f6ad865929121f5b80
shinchan0604 <190394302+shinchan0604@users.noreply.github.com>
2026-07-20 18:00:59 +0000

    put and delete

--- todo/app.js
+++ todo/app.js
@@ -66,6 +66,53 @@ app.post('/tasks', (req, res) => {
     res.status(201).json(newtask);
 })
 
+app.put('/tasks/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const task = tasks.find(task => task.id == id);
+    if(!task){
+        return res.status(404).json({
+            error:`task ${id} not found`
+        });
+    }
+    const {title, done} = req.body;
+    if(title === undefined && done === undefined){
+        return res.status(400).json({
+            error:"nothing to update"
+        });
+    }
+    if(title !== undefined){
+        if(typeof title !== "string" || title.trim() === ""){
+            return res.status(400).json({
+                error:"title must be a string"
+            });
+        }
+        task.title = title.trim();
+    }
+
+    if(done !== undefined){
+        if(typeof done !== "boolean"){
+            return res.status(400).json({
+                error:"done should be a boolean value"
+            });
+        }
+        task.done = done;
+    }
+    res.json(task);
+})
+
+app.delete('/tasks/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const index = tasks.findIndex(task => task.id === id);
+
+    if(index == -1){
+        return res.status(400).json({
+            error:`task ${id} not found`
+        });
+    }
+    tasks.splice(index,1);
+    res.sendStatus(204);
+})
+
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`);
 })
